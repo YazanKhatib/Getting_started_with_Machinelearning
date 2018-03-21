b1267fd8c18cac57392d0c51adee14f6385778ec
YazanKhatib <yazankhatib97@gmail.com>
2018-03-21 20:38:54 +0200

    Initial commit

---
+++ SimpleClassifier.py
@@ -0,0 +1,44 @@
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier() 
+
+features = [[50 , 0 ] , [ 80, 0 ] , [120 , 1 ] , [160, 1] , [180 , 2] , [200 ,2] ] 
+
+lables = ['honda' , 'honda' , 'audi' , 'audi' , 'ferrari', 'ferrai']
+
+clf = clf.fit(features, lables) 
+
+prediction = clf.predict([[90, 1]]) 
+
+print (prediction)
+
+
+"""
+Gender Classification challenge 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+
+# CHALLENGE - create 3 more classifiers...
+# 1
+# 2
+# 3
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+# CHALLENGE - ...and train them on our data
+clf = clf.fit(X, Y)
+
+prediction = clf.predict([[190, 70, 43]])
+
+# CHALLENGE compare their reusults and print the best one!
+
+print(prediction)
+"""
